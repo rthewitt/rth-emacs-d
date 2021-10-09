7c5c87e7d5ee9de1afc8d44ec90b0860106ae43b
Ryan Hewitt <rthewitt.devel@gmail.com>
2021-10-09 06:14:39 -0400

    Added VTerm - yet to compile

--- init.el
+++ init.el
@@ -15,4 +15,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(which-key evil-magit command-log-mode ivy-rich symex htmlize org-tempo use-package undo-tree projectile persistent-scratch org-bullets mixed-pitch markdown-mode lua-mode key-chord haskell-mode gnu-elpa-keyring-update general gcmh evil-surround evil-escape evil-commentary doom-themes doom-modeline darkroom company)))
+   '(vterm which-key evil-magit command-log-mode ivy-rich symex htmlize org-tempo use-package undo-tree projectile persistent-scratch org-bullets mixed-pitch markdown-mode lua-mode key-chord haskell-mode gnu-elpa-keyring-update general gcmh evil-surround evil-escape evil-commentary doom-themes doom-modeline darkroom company)))

7b5539453f69320e288a00b4ffd5d80e32da7eea
Ryan Hewitt <rthewitt.devel@gmail.com>
2023-01-18 21:26:41 -0500

    Removing org-mode conflicts after upgrade (init.el)

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
-(add-to-list 'load-path "~/source/org-mode/lisp")
-(require 'org-loaddefs)
-
 (org-babel-load-file
  (expand-file-name
   "config.org"
@@ -19,4 +16,4 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/org/agenda.org" "~/org/habits.org" "~/org/goals.org"))
  '(package-selected-packages
-   '(windresize cider clojure-mode dap-mode lsp-mode evil-collection vterm which-key evil-magit command-log-mode ivy-rich symex htmlize use-package undo-tree projectile persistent-scratch org-bullets mixed-pitch markdown-mode lua-mode key-chord haskell-mode gnu-elpa-keyring-update general gcmh evil-surround evil-escape evil-commentary doom-themes doom-modeline darkroom company)))
+   '(dash f s emacsql emacsql-sqlite3 windresize cider clojure-mode dap-mode lsp-mode evil-collection vterm which-key evil-magit command-log-mode ivy-rich symex htmlize use-package undo-tree projectile persistent-scratch org-bullets mixed-pitch markdown-mode lua-mode key-chord haskell-mode gnu-elpa-keyring-update general gcmh evil-surround evil-escape evil-commentary doom-themes doom-modeline darkroom company)))

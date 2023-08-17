deb9732557600e12cc3d65a3293649053965b23b
Ryan Hewitt <rthewitt.devel@gmail.com>
2023-08-17 17:20:41 -0400

    Agenda changes and updates (house, filters, super-agenda, etc)

--- init.el
+++ init.el
@@ -14,6 +14,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/org/agenda.org" "~/org/habits.org" "~/org/goals.org"))
+ '(org-agenda-files "~/org/projects.org")
  '(package-selected-packages
-   '(dash f s emacsql emacsql-sqlite3 windresize cider clojure-mode dap-mode lsp-mode evil-collection vterm which-key evil-magit command-log-mode ivy-rich symex htmlize use-package undo-tree projectile persistent-scratch org-bullets mixed-pitch markdown-mode lua-mode key-chord haskell-mode gnu-elpa-keyring-update general gcmh evil-surround evil-escape evil-commentary doom-themes doom-modeline darkroom company)))
+   '(org-super-agenda posframe org-ql dash f s emacsql emacsql-sqlite3 windresize cider clojure-mode dap-mode lsp-mode evil-collection vterm which-key evil-magit command-log-mode ivy-rich symex htmlize use-package undo-tree projectile persistent-scratch org-bullets mixed-pitch markdown-mode lua-mode key-chord haskell-mode gnu-elpa-keyring-update general gcmh evil-surround evil-escape evil-commentary doom-themes doom-modeline darkroom company)))

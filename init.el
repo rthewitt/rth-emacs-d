f49c0b05ff59479abc40f71b7711a304afc120fc
Ryan Hewitt <rthewitt.devel@gmail.com>
2023-08-21 16:00:45 -0400

    LaTex and PDF dependencies

--- init.el
+++ init.el
@@ -16,4 +16,4 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files "~/org/projects.org")
  '(package-selected-packages
-   '(org-super-agenda posframe org-ql dash f s emacsql emacsql-sqlite3 windresize cider clojure-mode dap-mode lsp-mode evil-collection vterm which-key evil-magit command-log-mode ivy-rich symex htmlize use-package undo-tree projectile persistent-scratch org-bullets mixed-pitch markdown-mode lua-mode key-chord haskell-mode gnu-elpa-keyring-update general gcmh evil-surround evil-escape evil-commentary doom-themes doom-modeline darkroom company)))
+   '(pdf-tools org-super-agenda posframe org-ql dash f s emacsql emacsql-sqlite3 windresize cider clojure-mode dap-mode lsp-mode evil-collection vterm which-key evil-magit command-log-mode ivy-rich symex htmlize use-package undo-tree projectile persistent-scratch org-bullets mixed-pitch markdown-mode lua-mode key-chord haskell-mode gnu-elpa-keyring-update general gcmh evil-surround evil-escape evil-commentary doom-themes doom-modeline darkroom company)))
